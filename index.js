055a16e2de4cb4fd0ea54f1b396d52bbaf298197
Kirk An <you@example.comkirkan95@gmail.com>
2021-02-11 07:29:10 -0500

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+//Calling and creating the function for pulling data from the COVID API
+$(document).ready(initialize);
+
+function initialize() {    
+//Making the data update every 10,000 milliseconds (10 seconds)
+// setInterval(getData, 10000);
+ }
+
+// const button = document.getElementsByClassName("btn btn-primary");
+// button.onClick = refreshPage()
+    
+// // function refreshPage (){
+// //     window.location.reload();
+// // }
+
+getData();
+async function getData(){
+    const api_url = 'https://corona-api.com/countries/US';
+    let response =  await fetch(api_url);
+    let dataset = await response.json();
+    let confirmed = dataset.data.today.confirmed;
+    let deaths = dataset.data.today.deaths;
+    let myTimeline = dataset.data.updated_at;
+    $('#conf').html(confirmed);
+    $('#deth').html(deaths);
+    $('#dataUpdated').html(new Date(myTimeline));
+    $('#userUpdated').html(new Date());
+}
